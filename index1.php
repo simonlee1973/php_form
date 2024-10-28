6b60f4c5a790a23748502485c6178413930f90db
simon.lee <chen2020713@gmail.com>
2024-10-28 12:20:43 +0800

    first commit

---
+++ index1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>計算BMI</title>
+</head>
+<body>
+    <h1>計算BMI</h1>
+    <form action="result.php" method="get">
+    <div>   
+        <label for="height">身高</label>    
+        <input type="number" name="height" id="height" step="0.1">
+    </div> 
+    <div>
+        <label for="height">體重</label>
+        <input type="text" name="weight" id="weight" step="0.1">
+    </div>
+    <div>
+
+
+        <input type="submit" value="計算">
+        <input type="reset" value="重置">
+    </div>
+    </form>
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMI結果</title>
+</head>
+<body>
+    <h1>BMI 結果</h1>
+<div>你的身高:<?=$_GET['height'];?> 公分</div>
+<div>你的體重:<?=$_GET['weight'];?> 公git 斤</div>
+<?php
+    $BMI=round($_GET['weight']*10000/($_GET['height'] *$_GET['height']),2);
+?>
+
+
+<div>你BMI 為:<?=$BMI; ?> </div>
+<?php
+        
+        if($BMI<18.5)
+            $result="太輕";
+        else if($BMI<24)
+            $result="正常";
+        else if($BMI<27)
+            $result="過重";    
+        else if($BMI<30)
+            $result="中度肥胖";
+        else if($BMI<35)
+            $result="中度肥胖";                           
+        else 
+        $result="重度肥胖";
+    ?>
+<div>你的體位判定:<?=$result?>
+
+    
+    
+</div>
+<div> <a href="index1.php">重首頁/重新輸入</a> 
+      </div>
+</body>
+
+</html>
